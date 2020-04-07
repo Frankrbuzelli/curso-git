a14433aabc0464151d07315f0682233a92237e1c
Frank Reinaldo <frankr.buzelli@icloud.com>
2020-04-07 16:44:07 -0300

    aula membro esta finalizada

---
+++ Aula46_MembrosEstaticos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula46_MembrosEstaticos
+{
+    class Program
+    {
+        static double Pi = 3.14;
+            static void Main(string[] args)
+            {
+            Console.WriteLine("Entre com o valor do Raio:  ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+
+            Console.WriteLine("O valor da circunferencia e: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+
+            double volume = Volume(raio);
+
+            Console.WriteLine("O volume é: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O valor de Pi é: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        static double Volume(double r)
+        {
+            return 4 / 3 * Pi * Math.Pow(r, 3.0);
+        }
+    
+    
+    
+    }
+}
